99d71d9987cd9ec81eda6fa2a223dd734bd1cbd1
Keks <keks@htmlacademy.ru>
2023-09-26 17:12:22 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/picture.js
@@ -0,0 +1,64 @@
+import { posts } from './data.js';
+import { isEscapeKey } from './util.js';
+import {
+  resetCommentShown,
+  setComments,
+  onCommentsLoaderClick
+} from './comments.js';
+
+const pictures = document.querySelector('.pictures');
+const pictureUserTemplate = document.querySelector('#picture')
+  .content.querySelector('.picture');
+const bigPicture = document.querySelector('.big-picture');
+const modalPhoto = bigPicture.querySelector('#modalphoto');
+const body = document.querySelector('body');
+const likesCount = document.querySelector('.likes-count');
+const commentsCount = document.querySelector('.social__comment-count');
+const photoDescription = document.querySelector('.social__caption');
+const closeButtonBigPicture = bigPicture.querySelector('.big-picture__cancel');
+
+const pictureListFragment = document.createDocumentFragment();
+
+const onBigPictureEscKeyDown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    bigPicture.classList.add('hidden');
+    body.classList.remove('modal-open');
+  }
+};
+
+const openBigPicture = (pictureElement, post) => {
+  pictureElement.addEventListener('click', () => {
+    modalPhoto.src = post.url;
+    likesCount.textContent = post.likes;
+    commentsCount.textContent = post.comments.length;
+    photoDescription.textContent = post.description;
+    resetCommentShown();
+    setComments(post);
+    onCommentsLoaderClick();
+    bigPicture.classList.remove('hidden');
+    body.classList.add('modal-open');
+    document.addEventListener('keydown', onBigPictureEscKeyDown);
+  });
+};
+
+//отрисовывает входящие данные
+posts.forEach((post) => {
+  const pictureElement = pictureUserTemplate.cloneNode(true);
+  pictureElement.querySelector('img').setAttribute('src', post.url);
+  pictureElement.querySelector('img').setAttribute('alt', post.description);
+  pictureElement.querySelector('.picture__likes').textContent = post.likes;
+  const comments = pictureElement.querySelector('.picture__comments');
+  comments.textContent = post.comments.length;
+  openBigPicture(pictureElement, post);
+  pictureListFragment.appendChild(pictureElement);
+});
+pictures.appendChild(pictureListFragment);
+
+const closeBigPicture = () => {
+  bigPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onBigPictureEscKeyDown);
+};
+
+closeButtonBigPicture.addEventListener('click', closeBigPicture);
